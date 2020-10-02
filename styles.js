923617d2574f14a15fc0b23c0b53db8127b2b4e6
Paul Hagwood <SubtleKnife@Pauls-MacBook-Pro.local>
2020-10-02 10:20:00 -0400

    saving text in blocks

--- styles.js
+++ styles.js
@@ -8,7 +8,7 @@ var presentHour = moment().hour();
 console.log('Hour ID is ', presentHour);
 
 // Set up what is a  workday
-var workDay = {
+var workHours = {
     "8 AM": "8",
     "9 AM": "9",
     "10 AM": "10",
@@ -21,15 +21,40 @@ var workDay = {
     "5 PM": "17",
   };
 
+
+
+//   Set Up WorkDay Content
+const workdayTasks = {
+    "8 AM": "",
+    "9 AM": "",
+    "10 AM": "",
+    "11 AM": "",
+    "12 PM": "",
+    "1 PM": "",
+    "2 PM": "",
+    "3 PM": "",
+    "4 PM": "",
+    "5 PM": "",
+}
+
+
+
+// var retrievedWorktasks = localStorage.getItem('workdayTasks');
+
+// console.log('retrieved work tasks are ', retrievedWorktasks);
+
+
+$(document).ready(function(){
 // Set the Date at the top
 $('#currentDay').text(day + ', ' + month + ' ' + dayOf);
 
-var objectKeys = Object.keys(workDay);
+// get the values of workday
+var objectKeys = Object.keys(workHours);
 console.log('workday object is ', objectKeys);
-
-for (property in workDay) {
+// loop through and add classes to the html
+for (property in workHours) {
     // console.log(`key = ${property} value = ${workDay[property]}`)
-    timeNumber = workDay[property];
+    timeNumber = workHours[property];
     console.log(timeNumber);
     textBlock = "#text-block" + timeNumber;
     console.log(textBlock);
@@ -41,3 +66,43 @@ for (property in workDay) {
         $(textBlock).addClass("present");
       }
 }
+
+// set up button to save text entered
+$('button').click(function() {
+    // get the hour text
+    hourEntered = $(this).siblings('.hour').text();
+    console.log("Hour entered is ", hourEntered);
+    // get the text the user entered
+    textEntered = $(this).siblings('textarea').val();
+    console.log("text entered is ", textEntered);
+
+    // save the hour and the text the user entered
+    saveSchedule(hourEntered, textEntered);
+});
+
+//   Put object into localstorage
+// function initializeLocalStorage (){
+//     localStorage.setItem('workdayTasks', JSON.stringify(workdayTasks));
+// }
+
+// send object to localStorage
+// function saveToLocalStorage(obj) {
+//     localStorage.setItem('workdayTasks', JSON.stringify(obj));
+// }
+
+// function to save the schedule
+function saveSchedule (hour, text){
+    if(!localStorage.getItem('workdayTasks')) {
+        initializeLocalStorage();
+    }
+
+    var workText = JSON.parse(localStorage.getItem('workdayTasks'));
+    workText[hour] = text;
+    console.log('workText is ', workText);
+
+    saveToLocalStorage(workText);
+
+}
+
+// End Document ready
+})
